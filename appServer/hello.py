e8e114223b9e0eb611b756ef6fb9a8e7fa9f0d96
KyleHersey <MAgus2UUUFuture@>
2019-05-22 17:31:25 -0700

    initial commit. Functioning hello world

---
+++ appServer/hello.py
@@ -0,0 +1,11 @@
+#FLASK_APP=hello.py flask run
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80, debug=False)
